1add88cda533b35c2c3c7ed309125a5d660cf5b9
mansikalathiya <kalathiyamansi1712@gmail.com>
2024-07-12 18:38:53 +0000

    .....

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ app.use(express.json());
 
 const PORT = 7000;
 
-
-
-
-
-
-
-
 app.post('/sum', (req, res) => {
     const { file, product } = req.body;
     console.log(`Received request to calculate sum for product: ${product} in file: ${file}`);
